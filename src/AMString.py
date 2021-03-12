d7da986f71da631a51a138256305447b32731b95
stephenhancock <stephen_hancock@btinternet.com>
2021-03-12 19:30:45 +0000

    corrected distance calc

--- src/AMString.py
+++ src/AMString.py
@@ -11,13 +11,7 @@ class AMString(object):
             # make a copy
             self.string = [{ch: pos[ch] for ch in pos} for pos in string.string]
         else:
-            for ch in string:
-                # add in opposite case
-                #
-                if ch.islower():
-                    self.string.append({ch: 1.0, ch.upper(): 0.5})
-                else:
-                    self.string.append({ch: 1.0, ch.lower(): 0.5})
+            self.string = [{ch: 1.0} for ch in string]
 
     def distance(self, string, normalise: Optional[int] = None) -> float:
         distance = 0.0
@@ -25,18 +19,9 @@ class AMString(object):
         if isinstance(string, AMString):
             str_to_compare = string.string
         else:
-            str_to_compare =[]
-            for ch in string:
-                # add in opposite case
-                #
-                if ch.islower():
-                    str_to_compare.append({ch: 1.0, ch.upper(): 0.5})
-                else:
-                    str_to_compare.append({ch: 1.0, ch.lower(): 0.5})
+            str_to_compare = [{ch: 1.0} for ch in string]
 
         string_len = max(len(self.string), len(str_to_compare))
-        if normalise is None:
-            normalise = string_len
 
         for pos in range(string_len):
             if pos < len(self.string) and pos < len(str_to_compare):
@@ -49,7 +34,7 @@ class AMString(object):
                         dist += self.string[pos][ch]
                     else:
                         dist += str_to_compare[pos][ch]
-                distance = dist / len(chars_to_test)
+                distance += dist / len(chars_to_test)
             elif pos < len(self.string):
                 dist = 0
                 for ch in self.string[pos]:
@@ -61,7 +46,9 @@ class AMString(object):
                     dist += str_to_compare[pos][ch]
                 distance += dist / len(str_to_compare[pos])
 
-        distance = distance / (normalise * 2)
+        if normalise is not None:
+            distance = distance / normalise
+
         return distance
 
     def learn(self, string, learn_rate: float = 1.0, prune=0.001):
@@ -95,7 +82,7 @@ class AMString(object):
                     if self.string[pos][ch] <= prune:
                         del self.string[pos][ch]
             else:
-                self.string.append({ch: str_to_learn[pos][ch] for ch in str_to_learn[pos]})
+                self.string.append({ch: str_to_learn[pos][ch] * learn_rate for ch in str_to_learn[pos]})
 
             if len(self.string[pos]) == 0:
                 pos_to_delete.append(pos)
@@ -105,8 +92,15 @@ class AMString(object):
                 del self.string[pos]
 
     def decode(self, threshold=0.8) -> str:
+
+        # get the maximum weight of any single character
+        #
         max_weight = max([pos[ch] for pos in self.string for ch in pos])
+
+        # calc cutoff
+        #
         cutoff = max_weight * threshold
+
         string = ''
 
         for pos in self.string:
@@ -119,6 +113,16 @@ class AMString(object):
 if __name__ == '__main__':
 
     max_string_size = 100
+
+    test_sentence = ['my', 'j', 'is', 'dog', 'hello', 'name',  'stephen']
+
+    test_strings = [AMString(s) for s in test_sentence]
+
+    amstr_to_compare = AMString('god')
+
+    distances = [amstr.distance(string=amstr_to_compare, normalise=max_string_size) for amstr in test_strings]
+
+
     s0 = AMString('H')
     d0_1 = s0.distance('h', normalise=max_string_size)
 
@@ -130,22 +134,27 @@ if __name__ == '__main__':
     s2 = AMString(s1)
     print(s2.decode())
 
-    s2.learn('Hello there')
+    s2.learn('Hello there', learn_rate=0.7)
     print(s2.decode())
 
-    s2.learn('Hello', learn_rate=0.9)
+    s2.learn('Hello', learn_rate=0.7)
     print(s2.decode())
+    print(s2.distance(string='Hello there'))
 
-    s2.learn('He', learn_rate=0.9)
+    s2.learn('Hello', learn_rate=0.7)
     print(s2.decode())
+    print(s2.distance(string='Hello there'))
 
-    s2.learn('He', learn_rate=0.9)
+    s2.learn('Hello', learn_rate=0.7)
     print(s2.decode())
+    print(s2.distance(string='Hello there'))
 
-    s2.learn('He', learn_rate=0.9)
+    s2.learn('Hello', learn_rate=0.7)
     print(s2.decode())
+    print(s2.distance(string='Hello there'))
 
-    s2.learn('He', learn_rate=0.9)
+    s2.learn('Hello', learn_rate=0.7)
     print(s2.decode())
+    print(s2.distance(string='Hello there'))
 
     print('finished')
